0d3a7b21e6825d07371c828469936574c4ce435e
salathiel wekesa <107699372+salawekesa@users.noreply.github.com>
2022-10-25 22:51:02 +0300

    reqs fail

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Reads a text file and prints it to stdout"""
+"""This module defines a text file-reading function"""
 
 
 def read_file(filename=""):
-    """prints out the text in the my_file_0.txt file"""
-    with open(filename, encoding="utf-8")as UTF8:
-        print(UTF8.read(),end="")
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
