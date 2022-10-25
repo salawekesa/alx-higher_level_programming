2b7a6a9f699becda5d4c22eba76884d759620d40
salawekesa <wekesa9235@gmail.com>
2022-10-25 19:38:18 +0000

    fxn that reads textfile

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Reads a text file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """prints out the text in the my_file_0.txt file"""
+    with open("my_file_0.txt", encoding="utf-8")as UTF8:
+        print(UTF8.read())
